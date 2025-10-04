d870f0abccb375e665b719ffa5494aac16a5dca6
cs2021hv001 <89976757+cs2021hv001@users.noreply.github.com>
2025-10-04 18:09:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-#tor &
-#/nezha -config /nezha.json &
-#caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
-
-tor > /dev/null 2>&1 &
-/nezha -config /nezha.json > /dev/null 2>&1 &
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile > /dev/null 2>&1
+/nezha -config /nezha.json
